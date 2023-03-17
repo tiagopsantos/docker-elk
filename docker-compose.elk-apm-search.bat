e16f29534fb797fa2a956122f6368804a9dacc62
Tiago Filipe Pereira dos Santos <tiago.santos@aitec.pt>
2023-03-17 18:34:05 +0000

    Start BAT + Elastic Search

---
+++ docker-compose.elk-apm-search.bat
@@ -0,0 +1,1 @@
+docker-compose  -f docker-compose.yml -f extensions/fleet/fleet-compose.yml -f extensions/fleet/agent-apmserver-compose.yml -f extensions/enterprise-search/enterprise-search-compose.yml up
